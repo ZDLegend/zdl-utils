8cd2b2dd3176ef8b1d3e301a2f87f78d1169ea65
张明浩 <463764941@qq.com>
2019-03-11 14:49:49 +0800

    Base64编码改为jdkapi

--- src/main/java/zdl/util/Base64Util.java
+++ src/main/java/zdl/util/Base64Util.java
@@ -1,25 +1,21 @@
 package zdl.util;
 
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
-
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class Base64Util {
-    private static final int CACHE_SIZE = 1024;
 
-    public static byte[] decode(String base64)
-            throws Exception {
-        return Base64.decode(base64);
+    public static byte[] decode(String base64) {
+        return Base64.getDecoder().decode(base64);
     }
 
-    public static String encode(byte[] bytes)
-            throws Exception {
-        return new String(Base64.encode(bytes));
+    public static String encode(byte[] bytes) {
+        return new String(Base64.getEncoder().encode(bytes));
     }
 
-    public static String encode(String str)
-            throws Exception {
-        return new String(Base64.encode(str.getBytes("UTF-8")));
+    public static String encode(String str) {
+        return new String(Base64.getEncoder().encode(str.getBytes(StandardCharsets.UTF_8)));
     }
 
     public static String encodeFile(String filePath)
@@ -39,37 +35,38 @@ public class Base64Util {
         byte[] data = new byte[0];
         File file = new File(filePath);
         if (file.exists()) {
-            FileInputStream in = new FileInputStream(file);
-            ByteArrayOutputStream out = new ByteArrayOutputStream(2048);
-            byte[] cache = new byte[1024];
-            int nRead = 0;
-            while ((nRead = in.read(cache)) != -1) {
-                out.write(cache, 0, nRead);
-                out.flush();
+            try(FileInputStream in = new FileInputStream(file);
+                ByteArrayOutputStream out = new ByteArrayOutputStream(2048)) {
+                byte[] cache = new byte[1024];
+                int nRead;
+                while ((nRead = in.read(cache)) != -1) {
+                    out.write(cache, 0, nRead);
+                    out.flush();
+                    data = out.toByteArray();
+                }
             }
-            out.close();
-            in.close();
-            data = out.toByteArray();
         }
         return data;
     }
 
     public static void byteArrayToFile(byte[] bytes, String filePath)
             throws Exception {
-        InputStream in = new ByteArrayInputStream(bytes);
+
         File destFile = new File(filePath);
         if (!destFile.getParentFile().exists()) {
             destFile.getParentFile().mkdirs();
         }
+        
         destFile.createNewFile();
-        OutputStream out = new FileOutputStream(destFile);
-        byte[] cache = new byte[1024];
-        int nRead = 0;
-        while ((nRead = in.read(cache)) != -1) {
-            out.write(cache, 0, nRead);
-            out.flush();
+
+        try(InputStream in = new ByteArrayInputStream(bytes);
+            OutputStream out = new FileOutputStream(destFile)) {
+            byte[] cache = new byte[1024];
+            int nRead;
+            while ((nRead = in.read(cache)) != -1) {
+                out.write(cache, 0, nRead);
+                out.flush();
+            }
         }
-        out.close();
-        in.close();
     }
 }
