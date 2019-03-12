96b063eedc66c70c9e9b37107d59a60de026d628
张明浩 <463764941@qq.com>
2019-03-12 14:48:29 +0800

    将类操作工具改为包操作工具，新建类操作工具

--- src/main/java/zdl/util/ClassUtil.java
+++ src/main/java/zdl/util/ClassUtil.java
@@ -1,200 +1,116 @@
 package zdl.util;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.JarURLConnection;
-import java.net.URL;
-import java.net.URLDecoder;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Objects;
 
 /**
  * @author ZDLegend
- * @create 2018/3/12
+ * @create 2019/3/12
  */
-
 public class ClassUtil {
 
     /**
-     * 通过包名获取包内所有类
+     * 获取一个类的注解,如果未获取到则获取父类
      *
-     * @param pkg
-     * @return
+     * @param clazz      要获取的类
+     * @param annotation 注解类型
+     * @param <T>        注解类型泛型
+     * @return 注解
      */
-    public static List<Class<?>> getAllClassByPackageName(Package pkg) {
-        // 获取当前包下以及子包下所以的类
-        return getClasses(pkg.getName());
-    }
-
-    /**
-     * 通过接口名取得某个接口下所有实现这个接口的类
-     */
-    public static List<Class<?>> getAllClassByInterface(Class<?> c) {
-        if (c.isInterface()) {
-            // 获取当前的包名
-            String packageName = c.getPackage().getName();
-            // 获取当前包下以及子包下所以的类
-            List<Class<?>> allClass = getClasses(packageName);
-            if (allClass != null) {
-                return allClass.stream()
-                        .distinct()
-                        .filter(c::isAssignableFrom)
-                        .filter(cls -> !c.equals(cls))
-                        .collect(Collectors.toList());
+    public static <T extends Annotation> T getAnnotation(Class<?> clazz, Class<T> annotation) {
+        T ann = clazz.getAnnotation(annotation);
+        if (ann != null) {
+            return ann;
+        } else {
+            if (clazz.getSuperclass() != Object.class) {
+                //尝试获取父类
+                return getAnnotation(clazz.getSuperclass(), annotation);
             }
         }
-
-        return null;
+        return ann;
     }
 
     /**
-     * 取得某一类所在包的所有类名 不含迭代
-     */
-    public static String[] getPackageAllClassName(String classLocation, String packageName) {
-        // 将packageName分解
-        String realClassLocation = String.join(File.separator, packageName.split("[.]"));
-        File packageDir = new File(realClassLocation);
-        if (packageDir.isDirectory()) {
-            return packageDir.list();
-        }
-        return null;
-    }
-
-    /**
-     * 从包package中获取所有的Class
+     * 获取一个方法的注解,如果未获取则获取父类方法
      *
-     * @param packageName 包名
+     * @param method     要获取的方法
+     * @param annotation 注解类型
+     * @param <T>        注解类型泛型
      * @return
      */
-    public static List<Class<?>> getClasses(String packageName) {
-
-        // 第一个class类的集合
-        List<Class<?>> classes = new ArrayList<>();
-        // 是否循环迭代
-        boolean recursive = true;
-        // 获取包的名字 并进行替换
-        String packageDirName = packageName.replace('.', '/');
-        // 定义一个枚举的集合 并进行循环来处理这个目录下的things
-        Enumeration<URL> dirs;
-        try {
-            dirs = Thread.currentThread().getContextClassLoader().getResources(packageDirName);
-            // 循环迭代下去
-            while (dirs.hasMoreElements()) {
-                // 获取下一个元素
-                URL url = dirs.nextElement();
-                // 得到协议的名称
-                String protocol = url.getProtocol();
-                // 如果是以文件的形式保存在服务器上
-                if ("file".equals(protocol)) {
-                    // 获取包的物理路径
-                    String filePath = URLDecoder.decode(url.getFile(), "UTF-8");
-                    // 以文件的方式扫描整个包下的文件 并添加到集合中
-                    findAndAddClassesInPackageByFile(packageName, filePath, recursive, classes);
-                } else if ("jar".equals(protocol)) {
-                    // 如果是jar包文件
-                    // 定义一个JarFile
-                    JarFile jar;
-                    try {
-                        // 获取jar
-                        jar = ((JarURLConnection) url.openConnection()).getJarFile();
-                        // 从此jar包 得到一个枚举类
-                        Enumeration<JarEntry> entries = jar.entries();
-                        // 同样的进行循环迭代
-                        while (entries.hasMoreElements()) {
-                            // 获取jar里的一个实体 可以是目录 和一些jar包里的其他文件 如META-INF等文件
-                            JarEntry entry = entries.nextElement();
-                            String name = entry.getName();
-                            // 如果是以/开头的
-                            if (name.charAt(0) == '/') {
-                                // 获取后面的字符串
-                                name = name.substring(1);
-                            }
-                            // 如果前半部分和定义的包名相同
-                            if (name.startsWith(packageDirName)) {
-                                int idx = name.lastIndexOf('/');
-                                // 如果以"/"结尾 是一个包
-                                if (idx != -1) {
-                                    // 获取包名 把"/"替换成"."
-                                    packageName = name.substring(0, idx).replace('/', '.');
-                                }
-                                // 如果可以迭代下去 并且是一个包
-                                // 如果是一个.class文件 而且不是目录
-                                if (name.endsWith(".class") && !entry.isDirectory()) {
-                                    // 去掉后面的".class" 获取真正的类名
-                                    String className = name.substring(packageName.length() + 1, name.length() - 6);
-                                    try {
-                                        // 添加到classes
-                                        classes.add(Class.forName(packageName + '.' + className));
-                                    } catch (ClassNotFoundException e) {
-                                        //TODO:
-                                        e.printStackTrace();
-                                    }
-                                }
-                            }
-                        }
-                    } catch (IOException e) {
-                        //TODO:
-                        e.printStackTrace();
-                    }
-                }
-            }
-        } catch (IOException e) {
-            //TODO:
-            e.printStackTrace();
-        }
-
-        return classes;
-    }
-
-    /**
-     * 以文件的形式来获取包下的所有Class
-     *
-     * @param packageName
-     * @param packagePath
-     * @param recursive
-     * @param classes
-     */
-    private static void findAndAddClassesInPackageByFile(String packageName, String packagePath,
-                                                         final boolean recursive, List<Class<?>> classes) {
-        // 获取此包的目录 建立一个File
-        File dir = new File(packagePath);
-        // 如果不存在或者 也不是目录就直接返回
-        if (!dir.exists() || !dir.isDirectory()) {
-            return;
-        }
-        // 如果存在 就获取包下的所有文件 包括目录
-        // 自定义过滤规则 如果可以循环(包含子目录) 或则是以.class结尾的文件(编译好的java类文件)
-        File[] dirfiles = dir.listFiles(file -> (recursive && file.isDirectory()) || (file.getName().endsWith(".class")));
-        if(dirfiles == null){
-            return;
-        }
-        // 循环所有文件
-        for (File file : dirfiles) {
-            // 如果是目录 则继续扫描
-            if (file.isDirectory()) {
-                findAndAddClassesInPackageByFile(packageName + "." + file.getName(), file.getAbsolutePath(), recursive, classes);
-            } else {
-                // 如果是java类文件 去掉后面的.class 只留下类名
-                String className = file.getName().substring(0, file.getName().length() - 6);
+    public static <T extends Annotation> T getAnnotation(Method method, Class<T> annotation) {
+        T ann = method.getAnnotation(annotation);
+        if (ann != null) {
+            return ann;
+        } else {
+            Class clazz = method.getDeclaringClass();
+            Class superClass = clazz.getSuperclass();
+            if (superClass != Object.class) {
                 try {
-                    // 添加到集合中去
-                    classes.add(Class.forName(packageName + '.' + className));
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
+                    //父类方法
+                    Method suMethod = superClass.getMethod(method.getName(), method.getParameterTypes());
+                    return getAnnotation(suMethod, annotation);
+                } catch (NoSuchMethodException e) {
+                    return null;
                 }
             }
         }
+        return ann;
     }
 
-    public static void main(String[] args) {
-        // 获取特定包下所有的类(包括接口和类)
-        List<Class<?>> clsList = ClassUtil.getClasses("zdl.es");
-        System.out.println(clsList);
+    public static Class<?> getGenericTypeByType(ParameterizedType genType, int index) {
+        Type[] params = genType.getActualTypeArguments();
+        if (index >= params.length || index < 0) {
+            return null;
+        }
+        Object res = params[index];
+        if (res instanceof Class) {
+            return ((Class) res);
+        }
+        if (res instanceof ParameterizedType) {
+            return (Class<?>) ((ParameterizedType) res).getRawType();
+        }
+        return null;
+    }
+
+    /**
+     * 获取一个类的泛型类型,如果未获取到返回Object.class
+     *
+     * @param clazz 要获取的类
+     * @param index 泛型索引
+     * @return 泛型
+     */
+    public static Class<?> getGenericType(Class clazz, int index) {
+        List<Type> arrys = new ArrayList<>();
+        arrys.add(clazz.getGenericSuperclass());
+        arrys.addAll(Arrays.asList(clazz.getGenericInterfaces()));
+        return arrys.stream()
+                .filter(Objects::nonNull)
+                .map(type -> {
+                    if (clazz != Object.class && !(type instanceof ParameterizedType)) {
+                        return getGenericType(clazz.getSuperclass(), index);
+                    }
+                    return getGenericTypeByType(((ParameterizedType) type), index);
+                })
+                .filter(Objects::nonNull)
+                .filter(res -> res != Object.class)
+                .findFirst()
+                .orElse((Class) Object.class);
+    }
+
+    /**
+     * 获取一个类的第一个泛型的类型
+     *
+     * @param clazz 要获取的类
+     * @return 泛型
+     */
+    public static Class<?> getGenericType(Class clazz) {
+        return getGenericType(clazz, 0);
     }
 }
