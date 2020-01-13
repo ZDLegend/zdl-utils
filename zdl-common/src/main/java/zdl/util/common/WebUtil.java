461307dab6e27992768181890b1d2ef0af04b08c
张明浩 <463764941@qq.com>
2020-01-13 09:59:02 +0800

    add webUtil

---
+++ zdl-common/src/main/java/zdl/util/common/WebUtil.java
@@ -0,0 +1,72 @@
+package zdl.util.common;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * web相关util
+ *
+ * Created by ZDLegend on 2020/1/13 9:49
+ */
+public class WebUtil {
+
+    private static final String[] ipHeaders = {
+            "X-Forwarded-For",
+            "X-Real-IP",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP"
+    };
+
+    public static Map<String, String> getParameters(HttpServletRequest request) {
+        Map<String, String> parameters = new HashMap<>();
+        Enumeration enumeration = request.getParameterNames();
+        while (enumeration.hasMoreElements()) {
+            String name = String.valueOf(enumeration.nextElement());
+            parameters.put(name, request.getParameter(name));
+        }
+        return parameters;
+    }
+
+    public static Map<String, String> getHeaders(HttpServletRequest request) {
+        Map<String, String> map = new LinkedHashMap<>();
+        Enumeration<String> enumeration = request.getHeaderNames();
+        while (enumeration.hasMoreElements()) {
+            String key = enumeration.nextElement();
+            String value = request.getHeader(key);
+            map.put(key, value);
+        }
+        return map;
+    }
+
+    /**
+     * 获取请求客户端的真实ip地址
+     *
+     * @param request 请求对象
+     * @return ip地址
+     */
+    public static String getIpAddr(HttpServletRequest request) {
+        for (String ipHeader : ipHeaders) {
+            String ip = request.getHeader(ipHeader);
+            if (StringUtils.isAllBlank(ip) && !ip.contains("unknown")) {
+                return ip;
+            }
+        }
+        return request.getRemoteAddr();
+    }
+
+    /**
+     * web应用绝对路径
+     *
+     * @param request 请求对象
+     * @return 绝对路径
+     */
+    public static String getBasePath(HttpServletRequest request) {
+        String path = request.getContextPath();
+        return request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + path + "/";
+    }
+}
