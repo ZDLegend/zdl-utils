a9e177e901da2440cfeb9c6e65bbffaa5414fac0
张明浩 <463764941@qq.com>
2020-03-21 16:21:40 +0800

    Create ContextUtil.java

---
+++ zdl-springboot-example/zdl-springboot-utils/src/main/java/zdl/springboot/utils/ContextUtil.java
@@ -0,0 +1,46 @@
+package zdl.springboot.utils;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.EmbeddedValueResolverAware;
+import org.springframework.util.StringValueResolver;
+
+/**
+ * Created by ZDLegend on 2020/3/21 16:13
+ */
+public class ContextUtil implements ApplicationContextAware, EmbeddedValueResolverAware {
+
+    private static ApplicationContext context;
+    private static StringValueResolver resolver;
+    private static final String KEY_FORMAT = "${%s}";
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        if (applicationContext == null) {
+            context = applicationContext;
+        }
+    }
+
+    @Override
+    public void setEmbeddedValueResolver(StringValueResolver stringValueResolver) {
+        if (stringValueResolver == null) {
+            resolver = stringValueResolver;
+        }
+    }
+
+    public static Object getBean(String name) {
+        return context.getBean(name);
+    }
+
+    public static <T> T getBean(Class<T> tClass) {
+        return context.getBean(tClass);
+    }
+
+    public static <T> T getBean(String name, Class<T> tClass) {
+        return context.getBean(name, tClass);
+    }
+
+    public static String getProperty(String key) {
+        return resolver.resolveStringValue(String.format(KEY_FORMAT, key));
+    }
+}
