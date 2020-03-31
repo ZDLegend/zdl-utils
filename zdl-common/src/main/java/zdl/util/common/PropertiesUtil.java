02159c9d2d76eeaf8bc6ec04a037f63b06ea95f4
张明浩 <463764941@qq.com>
2020-03-31 18:11:33 +0800

    添加自定义配置文件加载

--- zdl-common/src/main/java/zdl/util/common/PropertiesUtil.java
+++ zdl-common/src/main/java/zdl/util/common/PropertiesUtil.java
@@ -1,6 +1,13 @@
 package zdl.util.common;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+import java.util.Properties;
 import java.util.ResourceBundle;
+import java.util.stream.Stream;
 
 /**
  * Created by ZDLegend on 2020/3/26 16:55
@@ -9,7 +16,31 @@ public class PropertiesUtil {
 
     public static final ResourceBundle RESOURCE = ResourceBundle.getBundle("application");
 
+    public static final Properties PROP = new Properties();
+
+    static {
+        String files = getStringByKey("files");
+        if(StringUtils.isNoneBlank()) {
+
+            Stream.of(files.trim().split(","))
+                    .map(name -> PropertiesUtil.class.getClassLoader().getResourceAsStream(name))
+                    .filter(Objects::nonNull)
+                    .map(InputStreamReader::new)
+                    .forEach(reader -> {
+                        try {
+                            PROP.load(reader);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        }
+    }
+
     public static String getStringByKey(String key) {
         return RESOURCE.getString(key);
+    }
+
+    public static String getPropKey(String key) {
+        return PROP.getProperty(key);
     }
 }
