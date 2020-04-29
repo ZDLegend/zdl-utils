96e6c0d90fd802b272de6dce1fac3351e53567a8
ZDLegend <463764941@qq.com>
2020-04-29 11:23:22 +0800

    Update Solution.java

--- zdl-leetcode/src/main/java/zdl/util/leetcode/Solution.java
+++ zdl-leetcode/src/main/java/zdl/util/leetcode/Solution.java
@@ -8,11 +8,14 @@ import java.util.HashMap;
 public class Solution {
 
     /**
+     * 第一题
+     *
      * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
      * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
-     * example: 给定 nums = [2, 7, 11, 15], target = 9
-     *          因为 nums[0] + nums[1] = 2 + 7 = 9
-     *          所以返回 [0, 1]
+     * example:
+     * 给定 nums = [2, 7, 11, 15], target = 9
+     * 因为 nums[0] + nums[1] = 2 + 7 = 9
+     * 所以返回 [0, 1]
      */
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> table = new HashMap<>();
@@ -24,5 +27,49 @@ public class Solution {
             }
         }
         return new int[]{};
+    }
+
+    /**
+     * 第二题
+     *
+     * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+     * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+     * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     * example:
+     * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * 输出：7 -> 0 -> 8
+     * 原因：342 + 465 = 807
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int i = 0;
+        ListNode head = null;
+        ListNode next = null;
+        while (l1 != null || l2 != null || i != 0) {
+            int val1 = l1 == null ? 0 : l1.val;
+            int val2 = l2 == null ? 0 : l2.val;
+            int value = val1 + val2 + i;
+            ListNode node = new ListNode(value % 10);
+            i = value / 10;
+            if(head == null) {
+                head = node;
+                next = node;
+            } else {
+                next.next = node;
+            }
+
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+
+        return head;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
     }
 }
