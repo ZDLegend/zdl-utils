f1cd8f29ee866e5255591e9a3419f0669994c3f6
ZDLegend <463764941@qq.com>
2020-06-05 14:48:38 +0800

    添加Linux环境命令行方式强制关闭系统方法

--- zdl-springboot-example/zdl-springboot-utils/src/main/java/zdl/springboot/utils/ContextUtil.java
+++ zdl-springboot-example/zdl-springboot-utils/src/main/java/zdl/springboot/utils/ContextUtil.java
@@ -7,6 +7,9 @@ import org.springframework.context.EmbeddedValueResolverAware;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringValueResolver;
 
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
 /**
  * Created by ZDLegend on 2020/3/21 16:13
  */
@@ -47,9 +50,25 @@ public class ContextUtil implements ApplicationContextAware, EmbeddedValueResolv
         return resolver.resolveStringValue(String.format(KEY_FORMAT, key));
     }
 
+    /**
+     * Spring 方式退出系统
+     */
     public static void shutdown() {
         if (context instanceof ConfigurableApplicationContext) {
             ((ConfigurableApplicationContext) context).close();
+        }
+    }
+
+    /**
+     * Linux环境命令行方式强制关闭系统
+     */
+    public static void forceCloseSys() {
+        String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
+        String exec = String.format("kill -9 %s", pid);
+        try {
+            Runtime.getRuntime().exec(exec);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
