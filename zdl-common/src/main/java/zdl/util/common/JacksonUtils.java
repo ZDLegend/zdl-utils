4969955f93a38ba0ddb2b3cdc27260f38c95c9eb
ZDLegend <463764941@qq.com>
2020-07-09 16:51:03 +0800

    添加使用json schema检查json字符串

--- zdl-common/src/main/java/zdl/util/common/JacksonUtils.java
+++ zdl-common/src/main/java/zdl/util/common/JacksonUtils.java
@@ -1,8 +1,14 @@
 package zdl.util.common;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.networknt.schema.JsonSchema;
 import com.networknt.schema.JsonSchemaFactory;
 import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
+
+import java.util.Set;
 
 /**
  * Created by ZDLegend on 2020/5/22 11:22
@@ -10,6 +16,17 @@ import com.networknt.schema.SpecVersion;
 public class JacksonUtils {
 
     public static final JsonSchemaFactory schemaFactory = JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V7);
+
+    public static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static JsonNode validate(JsonSchema jsonSchema, String json) throws JsonProcessingException {
+        JsonNode jsonNode = objectMapper.readValue(json, JsonNode.class);
+        Set<ValidationMessage> validationMessages = jsonSchema.validate(jsonNode);
+        if (null != validationMessages && !validationMessages.isEmpty()) {
+            throw new RuntimeException(validationMessages.toString());
+        }
+        return jsonNode;
+    }
 
     public static void main(String[] args) {
         JsonSchema configSchema =
