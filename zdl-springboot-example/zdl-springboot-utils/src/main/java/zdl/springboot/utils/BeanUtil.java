551a83065c6c7f97eb73217bbfdb347197077e02
暗浩 <463764941@qq.com>
2020-09-15 10:24:24 +0800

    BeanUtil添加convert方法

--- zdl-springboot-example/zdl-springboot-utils/src/main/java/zdl/springboot/utils/BeanUtil.java
+++ zdl-springboot-example/zdl-springboot-utils/src/main/java/zdl/springboot/utils/BeanUtil.java
@@ -2,14 +2,40 @@ package zdl.springboot.utils;
 
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.util.CollectionUtils;
 
 import java.beans.FeatureDescriptor;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
  * Created by ZDLegend on 2020/3/24 14:41
  */
 public class BeanUtil extends org.springframework.beans.BeanUtils {
+
+    private BeanUtil() {
+    }
+
+    public static <T> List<T> convertList(List<?> source, Class<T> clazz) {
+        if (CollectionUtils.isEmpty(source)) {
+            return Collections.emptyList();
+        }
+        return source.stream().map(model -> convert(model, clazz)).collect(Collectors.toList());
+    }
+
+    public static <T> T convert(Object src, Class<T> clazz) {
+        T target = null;
+        try {
+            target = clazz.getDeclaredConstructor().newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assert target != null;
+        copyProperties(src, target);
+        return target;
+    }
 
     /**
      * copy scr中非null的属性至target中
