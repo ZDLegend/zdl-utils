a7b08115df6208003ffa91b6b4dc7fb5d7a75f2b
暗浩 <463764941@qq.com>
2020-09-17 11:11:35 +0800

    正则表达式匹配两个指定字符串中间的内容

--- zdl-common/src/main/java/zdl/util/common/StringUtils.java
+++ zdl-common/src/main/java/zdl/util/common/StringUtils.java
@@ -483,4 +483,37 @@ public final class StringUtils {
         }
         return sb.toString();
     }
+
+    /**
+     * 正则表达式匹配两个指定字符串中间的内容
+     */
+    public static List<String> getSubUtil(String soap, String rgex){
+        List<String> list = new ArrayList<>();
+        Pattern pattern = Pattern.compile(rgex);// 匹配的模式
+        Matcher m = pattern.matcher(soap);
+        while (m.find()) {
+            int i = 1;
+            list.add(m.group(i));
+        }
+        return list;
+    }
+
+    /**
+     * 返回单个字符串，若匹配到多个的话就返回第一个，方法与getSubUtil一样
+     */
+    public static String getSubUtilSimple(String soap,String rgex){
+        Pattern pattern = Pattern.compile(rgex);// 匹配的模式
+        Matcher m = pattern.matcher(soap);
+        while(m.find()){
+            return m.group(1);
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String str = "abc3443abcfgjhgabcgfjabc";
+        String rgex = "abc(.*?)abc";
+        System.out.println(getSubUtil(str,rgex));
+        System.out.println(getSubUtilSimple(str, rgex));
+    }
 }
