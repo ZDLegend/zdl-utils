064ec5416e589d37708832a248dd0bcf7f893291
暗浩 <463764941@qq.com>
2020-09-19 10:10:52 +0800

    Optimize imports

--- zdl-common/src/main/java/zdl/util/common/StringUtils.java
+++ zdl-common/src/main/java/zdl/util/common/StringUtils.java
@@ -5,11 +5,8 @@ import com.alibaba.fastjson.JSONArray;
 import java.io.*;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -486,7 +483,7 @@ public final class StringUtils {
     /**
      * 正则表达式匹配两个指定字符串中间的内容
      */
-    public static List<String> getSubUtil(String soap, String rgex){
+    public static List<String> getSubUtil(String soap, String rgex) {
         List<String> list = new ArrayList<>();
         Pattern pattern = Pattern.compile(rgex);// 匹配的模式
         Matcher m = pattern.matcher(soap);
@@ -500,10 +497,10 @@ public final class StringUtils {
     /**
      * 返回单个字符串，若匹配到多个的话就返回第一个，方法与getSubUtil一样
      */
-    public static String getSubUtilSimple(String soap,String rgex){
+    public static String getSubUtilSimple(String soap, String rgex) {
         Pattern pattern = Pattern.compile(rgex);// 匹配的模式
         Matcher m = pattern.matcher(soap);
-        while(m.find()){
+        while (m.find()) {
             return m.group(1);
         }
         return "";
@@ -512,7 +509,7 @@ public final class StringUtils {
     public static void main(String[] args) {
         String str = "abc3443abcfgjhgabcgfjabc";
         String rgex = "abc(.*?)abc";
-        System.out.println(getSubUtil(str,rgex));
+        System.out.println(getSubUtil(str, rgex));
         System.out.println(getSubUtilSimple(str, rgex));
     }
 }
